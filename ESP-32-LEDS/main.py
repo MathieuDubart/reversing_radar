073eea356948512ad54452c8667d40a28f0e0ffe
digital <digital@MacMini-6.local>
2023-05-17 08:50:27 +0200

    adding lowParam as param to bleManager()

--- ESP-32-LEDS/main.py
+++ ESP-32-LEDS/main.py
@@ -1,28 +1,28 @@
-# from bluetooth_manager import *
+from bluetooth_manager import *
 
-# # remplacer central par inbstance BTManager
-# bleManager = BluetoothManager()
+# remplacer central par inbstance BTManager
+bleManager = BluetoothManager(lowParam = 40)
 
-# try:
-#   bleManager.connect()
-#   while True:
-#     if bleManager.central.is_connected():
-#       bleManager.receive()
+try:
+  bleManager.connect()
+  while True:
+    if bleManager.central.is_connected():
+      bleManager.receive()
 
-# except KeyboardInterrupt:
-#     pass
+except KeyboardInterrupt:
+    pass
 
-import machine, neopixel
+# import machine, neopixel
 
-nbLeds = 4
-pin = 27
+# nbLeds = 4
+# pin = 27
 
-np = neopixel.NeoPixel(machine.Pin(p), n)
+# np = neopixel.NeoPixel(machine.Pin(p), n)
 
-np[0] = (255, 0, 0)
-np[1] = (125, 204, 223)
-np[2] = (120, 153, 23)
-np[3] = (255, 0, 153)
+# np[0] = (255, 0, 0)
+# np[1] = (125, 204, 223)
+# np[2] = (120, 153, 23)
+# np[3] = (255, 0, 153)
 
-while True:
-    np.write()
+# while True:
+#     np.write()
