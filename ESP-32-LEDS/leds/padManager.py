24d292d1091d24722c9f142929b8820913fa04de
digital <digital@MacMini-2.local>
2023-06-01 15:06:40 +0200

    fixing vibratino motor state machine

--- ESP-32-LEDS/leds/padManager.py
+++ ESP-32-LEDS/leds/padManager.py
@@ -1,5 +1,4 @@
 from padStates import *
-from machine import Pin
 from ledManager import *
 from motorManager import *
 
@@ -16,40 +15,33 @@ class PadManager:
     self.vibrationMotor = MotorManager(motorPin = 26)
     self.ledsColors = [(0, 255, 0), (255, 204, 0), (255, 70, 0), (255, 0, 0)]
 
-  def __ledStateManagement(self, newState, index):
+  def __stateManagement(self, newState, index):
     self.sensorsLeds[index].__updateState(newState)
     self.sensorsLeds[index].currentState.turnOnBand()
-  
-  def __motorStateManagement(self, newState):
-    self.vibrationMotor.__updateState(newState)
+    self.vibrationMotor.ledsStates[index] = self.sensorsLeds[index].currentState
     self.vibrationMotor.currentState.turnOnMotor()
-  
+
   def __stateChecking(self, valuesArray):
     i = 0
     while i < len(valuesArray):
       if int(valuesArray[i]) >= self.highParam:
-        self.__ledStateManagement(PadFarState(), index = i)
-        self.__motorStateManagement(MotorFarState())
+        self.__stateManagement(PadFarState(), index = i)
         print(int(valuesArray[i]),':',self.sensorsLeds[i].currentState)
 
       elif self.lowParam*1.25 < int(valuesArray[i]) < self.highParam:
-        self.__ledStateManagement(PadSemiFarState(), index = i)
-        self.__motorStateManagement(MotorFarState())
+        self.__stateManagement(PadSemiFarState(), index = i)
         print(int(valuesArray[i]),':',self.sensorsLeds[i].currentState)
 
       elif self.lowParam/2 < int(valuesArray[i]) < self.lowParam*1.25:
-        self.__ledStateManagement(PadSemiNearState(), index = i)
-        self.__motorStateManagement(MotorNearState())
+        self.__stateManagement(PadSemiNearState(), index = i)
         print(int(valuesArray[i]),':',self.sensorsLeds[i].currentState)
 
       elif 0 < int(valuesArray[i]) < self.lowParam/2:
-        self.__ledStateManagement(PadNearState(), index = i)
-        self.__motorStateManagement(MotorNearState())
+        self.__stateManagement(PadNearState(), index = i)
         print(int(valuesArray[i]),':',self.sensorsLeds[i].currentState)
 
       else:
-        self.__ledStateManagement(PadOutOfRangeState(), index = i)
-        self.__motorStateManagement(MotorNearState())
+        self.__stateManagement(PadOutOfRangeState(), index = i)
         print(int(valuesArray[i]),':',self.sensorsLeds[i].currentState)
 
       i+=1  
--- ESP-32-LEDS/motor/motorManager.py
+++ ESP-32-LEDS/motor/motorManager.py
@@ -1,13 +1,15 @@
-from machine import Pin
+from machine import PWM
 from motorStates import *
+from padStates import *
 
 class MotorManager:
   def __init__(self, motorPin):
-    self.motor = Pin(motorPin, Pin.OUT)
+    self.motor = PWM(motorPin, 1024)
     self.currentState = MotorInitialState()
 
   def __updateState(self, newState):
-    if str(self.currentState) != str(newState):
-      self.currentState = newState
-      self.currentState.context = self
-      print("New State: ", self.currentState)
+    if PadSemiNearState or PadNearState in self.ledsStates:
+      if str(self.currentState) != str(newState):
+        self.currentState = newState
+        self.currentState.context = self
+        print("New State: ", self.currentState)
